6667e6886507403619049b38d917bf0cfbd0d537
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-03-21 12:33:33 -0400

    added to commands class

--- src/PhysicalVolume.java
+++ src/PhysicalVolume.java
@@ -3,10 +3,13 @@ import java.util.ArrayList;
 public class PhysicalVolume extends RunnerHelp {
     private ArrayList<PhysicalHardDrive> pVList;
 
-    public PhysicalVolume(String pVName, PhysicalHardDrive pHD) {
+    public PhysicalVolume(String pVName) {
         super(pVName);
-        pVList.add(pHD);
     } //also has name, UUID
+
+    public void attachPVList(PhysicalHardDrive pHD) {
+        pVList.add(pHD);
+    }
 
     public String getpVName() { return super.getName(); }
     public ArrayList<PhysicalHardDrive> getpVListVList() { return pVList; }
