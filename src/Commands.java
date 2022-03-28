0e04145d631c238e26531a25bbac3c690d5b3681
Michelle Liu <michellel246@nycstudents.net>
2022-03-28 05:18:36 -0400

    added onto commands class for pv, phd and half of vg

--- src/Commands.java
+++ src/Commands.java
@@ -3,10 +3,18 @@ import java.util.ArrayList;
 public class Commands {
     private String command;
     private ArrayList<PhysicalHardDrive> pHDList;
+    private ArrayList<PhysicalHardDrive> pHDListUsed;
+    private ArrayList<PhysicalVolume> pVList;
+    private ArrayList<VolumeGroup> vGList;
+    private ArrayList<LogicalVolume> lVList;
 
     public Commands(String command) {
         this.command = command;
         pHDList = new ArrayList<PhysicalHardDrive>();
+        pHDListUsed = new ArrayList<PhysicalHardDrive>();
+        pVList = new ArrayList<PhysicalVolume>();
+        vGList = new ArrayList<VolumeGroup>();
+        lVList = new ArrayList<LogicalVolume>();
     }
 
     public void userCommand() {
@@ -22,17 +30,76 @@ public class Commands {
                 System.out.print(pHD.getName() + " [" + pHD.getSize() + "G]");
             }
         } else if (command.indexOf("pvcreate") == 0) {
-            String leftOver = command.substring(10);
+            String leftOver = command.substring(9);
             String volName = leftOver.substring(0, leftOver.indexOf(" "));
-            String size = leftOver.substring(volName.length(), leftOver.length() - 1);
+            String driveName = leftOver.substring(volName.length(), leftOver.length() - 1);
             PhysicalVolume pV = new PhysicalVolume(volName);
-
-
+            boolean exists = false;
+            for (PhysicalHardDrive pHD : pHDList) {
+                if (pHD.getName().equals(driveName)) {
+                    exists = true;
+                }
+            }
+            if (!exists) {
+                System.out.println("error: the drive does not exist");
+            } else {
+                boolean goOn = true;
+                for (PhysicalHardDrive pHD : pHDListUsed) {
+                    if (pHD.getName().equals(driveName)) {
+                        System.out.println("error: the drive is associated with another Physical Volume");
+                        goOn = false;
+                        break;
+                    }
+                }
+                for (PhysicalHardDrive pHDTest : pHDList) {
+                    if (pHDTest.getName().equals(driveName)) {
+                        PhysicalHardDrive pHD = pHDTest;
+                        pVList.add(pV);
+                        pHDListUsed.add(pHD);
+                        System.out.println(volName + " created");
+                        break;
+                    }
+                }
+            }
+        } else if (command.equals("pvlist")) {
+            for (int i = 0; i != pVList.size(); i++) {
+                PhysicalVolume pV = pVList.get(i);
+                PhysicalHardDrive pHD = pHDListUsed.get(i);
+                System.out.print(pV.getName() + ": [" + pHD.getSize() + "] [");
+                for (VolumeGroup vG: vGList) {
+                    if (vG.getpVList().contains(pV)) {
+                        System.out.print(vG.getName());
+                        break;
+                    }
+                }
+                System.out.println("] [" + pV.getUUID() + "]");
+            }
         } else if (command.indexOf("vgcreate") == 0) {
+            String leftOver = command.substring(9);
+            String volGName = leftOver.substring(0, leftOver.indexOf(" "));
+            String pVName = leftOver.substring(volGName.length(), leftOver.length() - 1);
+            PhysicalVolume pV = new PhysicalVolume(null);
+            boolean exists = false;
+            for (PhysicalVolume pVTest: pVList) {
+                if (pVTest.getName().equals(pVName)) {
+                    pV = pVTest;
+                    exists = true;
+                    break;
+                }
+            }
+            if (!exists) {
+                System.out.println("error: Physical Volume specified doesn't exist");
+            } else {
+                for (VolumeGroup vGTest: vGList) {
+                    if (vGTest.getpVList().contains(pV))
+                }
+            }
 
         } else if (command.equals("vglist")) {
 
-        } else if (command.equals("exit")) {
+        } else if (command.indexOf("lvcreate") == 0) {
+
+        } else if (command.equals("lvlist")) {
 
         } else {
             System.out.println("That is not in the list of your commands.");
--- src/PhysicalVolume.java
+++ src/PhysicalVolume.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 
 public class PhysicalVolume extends RunnerHelp {
-    private ArrayList<PhysicalHardDrive> pVList;
+    private ArrayList<PhysicalHardDrive> pHDList;
 
     public PhysicalVolume(String pVName) {
         super(pVName);
     } //also has name, UUID
 
-    public void attachPVList(PhysicalHardDrive pHD) {
-        pVList.add(pHD);
+    public void attachPHDList(PhysicalHardDrive pHD) {
+        pHDList.add(pHD);
     }
 
     public String getpVName() { return super.getName(); }
-    public ArrayList<PhysicalHardDrive> getpVListVList() { return pVList; }
+    public ArrayList<PhysicalHardDrive> getPHDList() { return pHDList; }
     public String getUUID() { return super.getUUIDuse(); }
 }
